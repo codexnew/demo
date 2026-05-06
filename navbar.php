a1f607e56d1cf5265cb9dce96c2dcdc5d2416d12
Rahat <rahat830611@gmail.com>
2026-05-06 10:32:11 +0600

    Revamp navbar with gradient background and icons

--- navbar.php
+++ navbar.php
@@ -1,18 +1,14 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm">
-  <div class="container">
-    <a class="navbar-brand fw-bold" href="#">Student LMS</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav ms-auto">
-        <li class="nav-item">
-          <a class="nav-link px-3" href="add_student.php">Add Student</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link px-3" href="view_student.php">View Student</a>
-        </li>
-      </ul>
+<nav class="navbar navbar-expand-lg navbar-dark bg-gradient shadow" style="background: linear-gradient(135deg, #667eea, #764ba2);">
+    <div class="container">
+        <a class="navbar-brand fw-bold fs-3" href="index.php">🎓 StudentHub</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link fw-semibold" href="add_student.php">➕ Add Student</a></li>
+                <li class="nav-item"><a class="nav-link fw-semibold" href="view_student.php">📋 View Students</a></li>
+            </ul>
+        </div>
     </div>
-  </div>
 </nav>
